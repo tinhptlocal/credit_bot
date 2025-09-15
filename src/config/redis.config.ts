dc39798b4a99a12b75efabc0e672d1a7afee9f52
tinhptNCC <tinh.phamthe@ncc.asia>
2025-09-15 14:12:52 +0700

    fix: deploy redis

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -5,14 +5,22 @@ import Redis from 'ioredis';
 export const redisClientFactory: FactoryProvider<Redis> = {
   provide: 'RedisClient',
   useFactory: (configService: ConfigService) => {
-    const redisInstance = new Redis({
-      host: configService.get<string>('REDIS.HOST') as string,
-      port: configService.get<number>('REDIS.PORT') as number,
+    const redisUrl = configService.get<string>('REDIS_URL');
+
+    if (!redisUrl) {
+      throw new Error('REDIS_URL is not defined in environment variables');
+    }
+
+    const redisInstance = new Redis(redisUrl, {
       maxRetriesPerRequest: null,
     });
 
+    redisInstance.on('connect', () => {
+      console.log('✅ Connected to Redis successfully');
+    });
+
     redisInstance.on('error', (e) => {
-      console.error(`Redis connection error: ${e.message}`);
+      console.error(`❌ Redis connection error: ${e.message}`);
     });
 
     return redisInstance;
